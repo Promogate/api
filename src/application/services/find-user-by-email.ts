24a67f347098ae0100e3a59a9a762cf0db8a3530
Augusto Dias <gutodiasux@gmail.com>
2023-04-15 11:18:58 -0300

    feat: add FindUserByEmailService

---
+++ src/application/services/find-user-by-email.ts
@@ -0,0 +1,20 @@
+import { UserNotFound } from '@/domain/error';
+import { FindUserByEmail } from '@/domain/features';
+import { UserRepository } from '@/infra/repositories';
+import { container, injectable } from 'tsyringe';
+
+@injectable()
+export class FindUserByEmailService implements FindUserByEmail {
+  async execute (input: FindUserByEmail.Input): Promise<FindUserByEmail.Output> {
+    const userRepository = container.resolve(UserRepository);
+    
+    const user = await userRepository.findByEmail(input);
+
+    if (user === null) {
+      throw new UserNotFound()
+    }
+
+    return user
+  }
+
+}
