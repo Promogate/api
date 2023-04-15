520cb0320dac7a078727d459e4db506f02a6c149
Augusto Dias <gutodiasux@gmail.com>
2023-04-15 16:23:16 -0300

    refact: remove error handler from service level since same verification is made in repository level

--- src/application/services/find-user-by-email.ts
+++ src/application/services/find-user-by-email.ts
@@ -1,5 +1,4 @@
 import { FindUserByEmailRepository } from '@/data/contracts';
-import { UserNotFound } from '@/domain/error';
 import { FindUserByEmail } from '@/domain/features';
 import { inject, injectable } from 'tsyringe';
 
@@ -8,19 +7,15 @@ export class FindUserByEmailService implements FindUserByEmail {
   constructor(
     @inject('UserRepository')
     private readonly userRepository: FindUserByEmailRepository
-  ) {}
+  ) { }
 
-  async execute (input: FindUserByEmail.Input): Promise<FindUserByEmail.Output> {    
+  async execute(input: FindUserByEmail.Input): Promise<FindUserByEmail.Output> {
     const user = await this.userRepository.findByEmail(input);
 
-    if (user === null) {
-      throw new UserNotFound()
-    }
-
     return {
-      id: user.id,
+      id: user.id as string,
       name: user.name ? user.name : null,
-      email: user.email
+      email: user.email as string
     }
   }
 
