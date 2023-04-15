59f8633434700ef3049fc67f425be2c8c9ec28c6
Augusto Dias <gutodiasux@gmail.com>
2023-04-15 10:50:10 -0300

    refact: remove singleton shared containers

--- src/main/shared/containers/index.ts
+++
@@ -1,5 +0,0 @@
-import { CreateUserRepository } from '@/data/contracts';
-import { UserRepository } from '@/infra/repositories';
-import { container } from 'tsyringe';
-
-container.registerSingleton<CreateUserRepository>('UserRepository',UserRepository);
