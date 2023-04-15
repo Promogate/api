91526fa3077d3ed3b56d7ab74252fc798f75a56f
Augusto Dias <gutodiasux@gmail.com>
2023-04-15 15:47:17 -0300

    feat: add CreateSession interface

---
+++ src/domain/features/create-session.ts
@@ -0,0 +1,14 @@
+export interface CreateSession {
+  execute: (input: CreateSession.Input) => Promise<CreateSession.Output>
+}
+
+export namespace CreateSession {
+  export type Input = {
+    email: string
+    password: string
+  }
+
+  export type Output = {
+    token: string
+  }
+}
