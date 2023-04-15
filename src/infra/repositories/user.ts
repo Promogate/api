e41249ab9960d73e3f359b75268e0d32266e038e
Augusto Dias <gutodiasux@gmail.com>
2023-04-15 08:54:58 -0300

    feat: implement CreateUserRepository interface

--- src/infra/repositories/user.ts
+++ src/infra/repositories/user.ts
@@ -1,3 +1,30 @@
-export class UserRepository {
-  
+import { CreateUserRepository } from '@/data/contracts';
+import { CreateUserFailed, UserAlredyExistsError } from '@/domain/error';
+import { prisma } from '@/main/config';
+
+export class UserRepository implements CreateUserRepository {
+  async create (input: CreateUserRepository.Input): Promise<CreateUserRepository.Ouput> {
+    try {
+      const userAlreadyExists = await prisma.user.findFirst({ where: { email: input.email } })
+
+      if (userAlreadyExists) {
+        throw new UserAlredyExistsError()
+      }
+
+      const user = await prisma.user.create({
+        data: {
+          name: input.name,
+          email: input.email,
+          password: input.password
+        }
+      })
+
+      return {
+        id: user.id
+      }
+    } catch (error: unknown) {
+      throw new CreateUserFailed()
+    }
+  }
+
 }
