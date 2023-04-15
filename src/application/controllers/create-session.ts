8cc33ffb0b116b0c335301299292752591ffd0d0
Augusto Dias <gutodiasux@gmail.com>
2023-04-15 16:15:33 -0300

    feat: add CreateSessionController

---
+++ src/application/controllers/create-session.ts
@@ -0,0 +1,13 @@
+import { CreateSessionService } from '@/application/services/';
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+class CreateSessionController {
+  async handle (req: Request, res: Response): Promise<Response> {
+    const createSessionService = container.resolve(CreateSessionService);
+    const result = await createSessionService.execute(req.body);
+    return res.status(200).json(result)
+  }
+}
+
+export const createSessionController = new CreateSessionController()
