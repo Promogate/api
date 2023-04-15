c6edee5625561c02af8df5a0ec18a51712521f02
Augusto Dias <gutodiasux@gmail.com>
2023-04-14 22:37:20 -0300

    chore: add express

--- src/main/api/index.ts
+++ src/main/api/index.ts
@@ -1,1 +1,6 @@
-console.log("Is working! with lint staged")
+import express from 'express';
+
+const app = express()
+app.use(express.json())
+
+app.listen(5000, () => console.log('Server is running on port: 5000'))
