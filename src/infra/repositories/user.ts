409370b86efc03f323414e06b03ced697517699f
Augusto Dias <gutodiasux@gmail.com>
2023-04-15 14:37:44 -0300

    feat: add findById method to user repository

--- src/infra/repositories/user.ts
+++ src/infra/repositories/user.ts
@@ -1,17 +1,17 @@
-import { CreateUserRepository, FindUserByEmailRepository } from '@/data/contracts';
-import { CreateUserFailed, UserAlredyExistsError } from '@/domain/error';
+import { CreateUserRepository, FindUserByEmailRepository, FindUserByIdRepository } from '@/data/contracts';
+import { CreateUserFailed, UserAlredyExistsError, UserNotFound } from '@/domain/error';
 import { prisma } from '@/main/config';
 import { injectable } from 'tsyringe';
 
 @injectable()
-export class UserRepository implements CreateUserRepository, FindUserByEmailRepository {
+export class UserRepository implements CreateUserRepository, FindUserByEmailRepository, FindUserByIdRepository {
   async create(input: CreateUserRepository.Input): Promise<CreateUserRepository.Ouput> {
     const userAlreadyExists = await this.findByEmail({ email: input.email })
 
     if (userAlreadyExists) {
       throw new UserAlredyExistsError()
     }
-    
+
     try {
 
       const user = await prisma.user.create({
@@ -33,7 +33,43 @@ export class UserRepository implements CreateUserRepository, FindUserByEmailRepo
   async findByEmail(input: FindUserByEmailRepository.Input): Promise<FindUserByEmailRepository.Output> {
     const user = await prisma.user.findFirst({ where: { email: input.email } })
 
-    return user
+    if (!user) {
+      throw new UserNotFound()
+    }
+
+    if (user.name) {
+      return {
+        id: user.id,
+        email: user.email,
+        name: user.name
+      }
+    }
+
+    return {
+      id: user.id,
+      email: user.email
+    }
+    
   }
 
+  async findById (input: FindUserByIdRepository.Input): Promise<FindUserByIdRepository.Output> {
+    const user = await prisma.user.findFirst({ where: { id: input.id } });
+
+    if (!user) {
+      throw new UserNotFound()
+    }
+
+    if (user.name) {
+      return {
+        id: user.id,
+        email: user.email,
+        name: user.name
+      }
+    }
+
+    return {
+      id: user.id,
+      email: user.email
+    }
+  }
 }
