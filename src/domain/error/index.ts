a73015d6ece11b485e02cdde46f8b817f2839d3a
Augusto Dias <gutodiasux@gmail.com>
2023-04-17 10:11:45 -0300

    feat: add SaveRefreshTokenError

--- src/domain/error/index.ts
+++ src/domain/error/index.ts
@@ -1,4 +1,5 @@
 export * from './authentication-failed';
 export * from './create-user-failed';
+export * from './save-refresh-token';
 export * from './user-already-exists';
 export * from './user-not-found';
