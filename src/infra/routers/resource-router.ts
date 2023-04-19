976d166e9c55616bf5d2f5d8295bfc8c652cece7
Augusto Dias <gutodiasux@gmail.com>
2023-04-19 19:41:19 -0300

    refact: change verifyAPIKey middleware to top level at resource router

--- src/infra/routers/resource-router.ts
+++ src/infra/routers/resource-router.ts
@@ -1,10 +1,12 @@
 import { createOfferController } from '@/application/controllers';
+import { listOffersController } from '@/application/controllers/list-offers';
 import { verifyAPIKey, verifyToken } from '@/application/middlewares';
 import { Router } from 'express';
 
 const resourceRouter = Router();
 
-resourceRouter.use(verifyToken);
-resourceRouter.post('/offer/create', verifyAPIKey, createOfferController.handle);
+resourceRouter.use(verifyToken, verifyAPIKey);
+resourceRouter.post('/offer/create', createOfferController.handle);
+resourceRouter.get('/offers', listOffersController.handle);
 
 export { resourceRouter };
