59ecb46af97bbd00222398fb5f2d349192a4e6d0
Augusto Dias <gutodiasux@gmail.com>
2023-04-19 21:41:42 -0300

    refact: change CreateApiKeyService to recieve user id directly from request

--- src/application/controllers/create-api-key.ts
+++ src/application/controllers/create-api-key.ts
@@ -3,9 +3,10 @@ import { Request, Response } from 'express';
 import { container } from 'tsyringe';
 
 class CreateApiKeyController {
-  async handle (req: Request, res: Response): Promise<Response> {
+  async handle (req: Request & { user?: string }, res: Response): Promise<Response> {
     const createApiKeyService = container.resolve(CreateApiKeyService);
-    const result = await createApiKeyService.execute(req.body);
+
+    const result = await createApiKeyService.execute({ id: req.user as string });
     return res.status(200).json(result);
   }
 }
