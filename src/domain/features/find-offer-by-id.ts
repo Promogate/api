79410b993c4e8f461cd92d255b6740a9cd535776
Augusto Dias <gutodiasux@gmail.com>
2023-04-23 11:14:14 -0300

    feat: add FindOfferById interface

---
+++ src/domain/features/find-offer-by-id.ts
@@ -0,0 +1,20 @@
+export interface FindOfferById {
+  execute: (input: FindOfferById.Input) => Promise<FindOfferById.Output>
+}
+
+export namespace FindOfferById {
+  export type Input = {
+    id: string,
+  }
+
+  export type Output = {
+    id: string,
+    title: string,
+    image: string,
+    old_price: string,
+    price: string,
+    destination_link: string,
+    store_image: string,
+    expiration_date: string,
+  }
+}
