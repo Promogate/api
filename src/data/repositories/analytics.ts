8aa0c018fcbd04d07d590d53738eb9a44656dfd2
Augusto Dias <gutodiasux@gmail.com>
2023-04-25 10:07:27 -0300

    refact: add resourceAnalytics new logic

--- src/data/repositories/analytics.ts
+++ src/data/repositories/analytics.ts
@@ -3,22 +3,29 @@ import { prisma } from '@/main/config';
 
 /*eslint-disable @typescript-eslint/no-explicit-any*/
 export class AnalyticsRepository implements AddOfferClickRepository {
-  async addClick (input: AddOfferClickRepository.Input): Promise<void> {
-    //
+  async addClick(input: AddOfferClickRepository.Input): Promise<void> {
     try {
       const offer = await prisma.offer.findUnique({
         where: { id: input.id }
       });
 
-      if (!offer) throw new Error('Offer not found...');
-
-      await prisma.offer.update({
-        where: { id: offer.id },
-        data: {
-          clicks: offer.clicks + 1
+      if (offer === null) throw new Error('Offer not found via analytics repository')
+      
+      const resourcesAnalytics = await prisma.resourcesAnalytics.findUnique({
+        where: {
+          resourcesId: offer.resourcesId
         }
       })
 
+      if (resourcesAnalytics === null) throw new Error('ResourceAnalytics not found via analytics repository')
+
+      await prisma.resourcesAnalyticsClicks.create({
+        data: {
+          resources_analytics_id: resourcesAnalytics.id,
+          offer_id: offer.id,
+          resource_id: offer.resourcesId
+        }
+      })
     } catch (error: any) {
       throw new Error('Failed to add click')
     }
