5b0516db31085de3933d4b5823b9a948b400b9ed
Augusto Dias <gutodiasux@gmail.com>
2023-04-26 09:42:51 -0300

    feat: add generateExpirationDate

---
+++ src/main/utils/generate-expiration-date.ts
@@ -0,0 +1,6 @@
+import dayjs, { ManipulateType } from 'dayjs';
+
+export function generateExpirationDate (value: number, unit: ManipulateType | undefined): string {
+  const now = dayjs()
+  return now.add(value, unit).format();
+}
--- src/main/utils/index.ts
+++ src/main/utils/index.ts
@@ -1,2 +1,3 @@
 export * from './express-error-handler';
 export * from './generate-api-key';
+export * from './generate-expiration-date';
