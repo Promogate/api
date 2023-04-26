bc6e7f9b84ff9ec1154e5d7597027c61c20404ce
Augusto Dias <gutodiasux@gmail.com>
2023-04-26 20:12:01 -0300

    feat: add GetOfferWithAnalytics interface

--- src/domain/features/index.ts
+++ src/domain/features/index.ts
@@ -7,6 +7,7 @@ export * from './find-offer-by-id';
 export * from './find-user-by-email';
 export * from './find-user-by-id';
 export * from './get-offers-clicks';
+export * from './get-offers-with-analytics';
 export * from './list-api-keys';
 export * from './list-offers';
 export * from './revalidate-refresh-token';
