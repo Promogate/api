b5e3caeeeca8124c2cec06ccdc02d9288009b4c1
Augusto Dias <gutodiasux@gmail.com>
2023-04-28 19:23:59 -0300

    feat: add csv importer route

--- src/infra/routers/testing.ts
+++ src/infra/routers/testing.ts
@@ -1,4 +1,5 @@
-import { convertCSVToJSONController } from '@/application/controllers';
+import { uploadOffersFromCSVConstroller } from '@/application/controllers';
+import { verifyToken } from '@/application/middlewares';
 import { Router } from 'express';
 import multer from 'multer';
 
@@ -7,6 +8,6 @@ const testingRoute = Router()
 const storage = multer.memoryStorage();
 const upload = multer({ storage });
 
-testingRoute.post('/parse', upload.single('file'), convertCSVToJSONController.handle)
+testingRoute.post('/parse', upload.single('file'), verifyToken, uploadOffersFromCSVConstroller.handle)
 
 export { testingRoute };
