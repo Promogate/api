5afee79b64b76b3633ecfc992e8c5d71b840186d
Augusto Dias <gutodiasux@gmail.com>
2023-04-28 09:35:28 -0300

    refact: make ConvertCSVToJSONService injectable

--- src/application/services/convert-csv-to-json.ts
+++ src/application/services/convert-csv-to-json.ts
@@ -1,6 +1,8 @@
 import { ConvertCSVToJSON } from '@/domain/features';
 import csvtojson from 'csvtojson';
+import { injectable } from 'tsyringe';
 
+@injectable()
 export class ConvertCSVToJSONService implements ConvertCSVToJSON {
   async execute(input: ConvertCSVToJSON.Input): Promise<ConvertCSVToJSON.Output> {
     const json = await csvtojson().fromString(input.csv.data.toString());
