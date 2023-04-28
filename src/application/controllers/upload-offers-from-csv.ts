3d82678c75dfae5045515a6b07ff21b11eba141a
Augusto Dias <gutodiasux@gmail.com>
2023-04-28 19:20:16 -0300

    refact: arrange correct names

--- src/application/controllers/upload-offers-from-csv.ts
+++ src/application/controllers/upload-offers-from-csv.ts
@@ -1,16 +1,17 @@
-import { UploadFileService } from '@/application/services';
+import { UploadOffersFromCSVService } from '@/application/services';
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
 
 class UploadOffersFromCSVConstroller {
   async handle(req: Request & { user?: string }, res: Response): Promise<Response> {
     const { file } = req;
-    const uploadFileService = container.resolve(UploadFileService)
+    const uploadOffersFromCSVService = container.resolve(UploadOffersFromCSVService)
 
-    const { json } = await uploadFileService.execute({ 
+    const { json } = await uploadOffersFromCSVService.execute({ 
       file: file as Express.Multer.File,
       user_id: req.user as string
     });
+    
     return res.status(200).json(json)
   }
 }
