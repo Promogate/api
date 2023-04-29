cc4ce471c4ee730c9656f278279cb99498b2f99d
Augusto Dias <gutodiasux@gmail.com>
2023-04-29 00:08:55 -0300

    refact: add token as return when creating a new user

--- src/application/services/create-user.ts
+++ src/application/services/create-user.ts
@@ -1,6 +1,8 @@
 import { CreateUserRepository } from '@/data/contracts';
 import { CreateUser } from '@/domain/features';
+import { TOKEN_SECRET } from '@/main/config';
 import { hash } from 'bcrypt';
+import { sign } from 'jsonwebtoken';
 import { inject, injectable } from 'tsyringe';
 
 @injectable()
@@ -13,6 +15,12 @@ export class CreateUserService implements CreateUser {
   async execute(input: CreateUser.Input): Promise<CreateUser.Output> {
     const hashedPassword = await hash(input.password, 10);
 
-    await this.userRepository.create({...input, password: hashedPassword});
+    const { user_id } = await this.userRepository.create({...input, password: hashedPassword});
+
+    const token = sign({ id: user_id }, TOKEN_SECRET, { expiresIn: '1d' });
+
+    return {
+      token
+    }
   }
 }
--- src/domain/features/create-user.ts
+++ src/domain/features/create-user.ts
@@ -9,5 +9,7 @@ export namespace CreateUser {
     password: string
   }
 
-  export type Output = void
+  export type Output = {
+    token: string
+  }
 }
