c58ac1fadbb74d78a1226e2cc32555507d150e81
Augusto Dias <gutodiasux@gmail.com>
2023-05-01 14:09:12 -0300

    refact: change api_key parse from request headers to a separated function at application layer

--- src/application/controllers/list-offers.ts
+++ src/application/controllers/list-offers.ts
@@ -1,9 +1,11 @@
 import { ListOffersService } from '@/application/services';
+import { getAPIKey } from '@/application/utils';
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
 
 class ListOffersController {
   async handle (req: Request & { user?: string }, res: Response): Promise<Response> {
+    const apiKey = getAPIKey(req)
     const listOffersService = container.resolve(ListOffersService);
     const result = await listOffersService.execute({ user_id: req.user as string });
     return res.status(200).json(result);
---
+++ src/application/utils/get-api-key.ts
@@ -0,0 +1,6 @@
+import { Request } from 'express';
+
+export function getAPIKey (request: Request): string {
+  const { "x-api-key": apiKey } = request.headers as { 'x-api-key': string };
+  return apiKey
+}
