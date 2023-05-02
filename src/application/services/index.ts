6830144fcc1d20a932fda7db7b1bece0cee3ca2e
Augusto Dias <gutodiasux@gmail.com>
2023-05-02 19:43:57 -0300

    feat: add sign-in service

--- src/application/services/index.ts
+++ src/application/services/index.ts
@@ -11,4 +11,5 @@ export * from './get-offers-clicks';
 export * from './get-offers-with-clicks-count';
 export * from './list-api-keys';
 export * from './list-offers';
+export * from './sign-in';
 export * from './upload-offers-from-csv';
---
+++ src/application/services/sign-in.ts
@@ -0,0 +1,26 @@
+import { ISignInRepo } from '@/data/contracts';
+import { ISignIn } from '@/domain/features';
+import { TOKEN_SECRET } from '@/main/config';
+import { sign } from 'jsonwebtoken';
+import { inject, injectable } from 'tsyringe';
+
+@injectable()
+export class SignInService implements ISignIn {
+  constructor(
+    @inject('AuthenticationRepository')
+    private readonly authenticationRepo: ISignInRepo
+  ) { }
+
+  async execute(input: ISignIn.Input): Promise<ISignIn.Output> {
+    const user = await this.authenticationRepo.signIn(input)
+    
+    const token = sign({ user: user.id }, TOKEN_SECRET, { expiresIn: '1d' })
+
+    return {
+      token,
+      user_profile: user.user_profile?.id ?? '',
+      user: user.id
+    }
+  }
+
+}
