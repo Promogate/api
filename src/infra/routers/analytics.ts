76cdad5c4c7ed11d2def823baab4697ec4fd6710
Augusto Dias <gutodiasux@gmail.com>
2023-05-04 18:56:50 -0300

    feat: add analytics profile route

--- src/infra/routers/analytics.ts
+++ src/infra/routers/analytics.ts
@@ -1,5 +1,10 @@
+import { getProfileController } from '@/application/controllers';
+import { verifyToken } from '@/application/middlewares';
 import { Router } from 'express';
 
 const analyticsRouter = Router();
+
+analyticsRouter.use(verifyToken);
+analyticsRouter.get('/profile/:id', getProfileController.handle);
 
 export { analyticsRouter };
