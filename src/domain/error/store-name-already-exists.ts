4f4865211b66e187232ae86afdbedeb62f3b82dc
Augusto Dias <gutodiasux@gmail.com>
2023-05-03 21:49:27 -0300

    feat: add StoreNameAlreadyExists error

---
+++ src/domain/error/store-name-already-exists.ts
@@ -0,0 +1,10 @@
+export class StoreNameAlreadyExists extends Error {
+  constructor() {
+    super('Nome da loja já existe');
+    this.name = 'StoreNameAlreadyExists'
+    return {
+      message: this.message,
+      name: this.name
+    }
+  }
+}
