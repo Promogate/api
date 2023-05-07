de84fac9a4a1c86c36561ee88b3c082b99633ac8
Augusto Dias <gutodiasux@gmail.com>
2023-05-07 16:50:54 -0300

    feat: add resources store route

--- src/infra/routers/resources.ts
+++ src/infra/routers/resources.ts
@@ -1,8 +1,12 @@
-import { getShowcaseOffersController } from '@/application/controllers';
+import {
+  getShowcaseOffersController,
+  getStoreDataController
+} from '@/application/controllers';
 import { Router } from 'express';
 
 const resourcesRouter = Router();
 
 resourcesRouter.get('/offers/:store', getShowcaseOffersController.handle);
+resourcesRouter.get('/store/:store', getStoreDataController.handle);
 
 export { resourcesRouter };
