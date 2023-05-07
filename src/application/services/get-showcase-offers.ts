6e01ae7d44fa71294b3579fdf36190a1cd4c6d17
Augusto Dias <gutodiasux@gmail.com>
2023-05-07 09:35:08 -0300

    feat: implement GetShowcaseOffersService

--- src/application/services/get-showcase-offers.ts
+++ src/application/services/get-showcase-offers.ts
@@ -1,10 +1,17 @@
+import { IGetShowcaseOffersRepo } from '@/data/contracts';
 import { IGetShowcaseOffers } from '@/domain/features';
-import { injectable } from 'tsyringe';
+import { inject, injectable } from 'tsyringe';
 
 @injectable()
 export class GetShowcaseOffersService implements IGetShowcaseOffers {
+  constructor(
+    @inject('ResourcesRepository')
+    private readonly resourcesRepo: IGetShowcaseOffersRepo
+  ) {}
+
   async execute(input: IGetShowcaseOffers.Input): Promise<IGetShowcaseOffers.Output> {
-    throw new Error('Method not implemented.');
+    const offers = await this.resourcesRepo.getOffers({ store_name: input.store_name });
+    return offers
   }
 
 }
