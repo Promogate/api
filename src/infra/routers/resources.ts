60eba147d317dbedfcb028c1cb3ff17a2c4c3705
Augusto Dias <gutodiasux@gmail.com>
2023-05-21 18:24:48 -0300

    refact: move get categories from a resource to a controller

--- src/infra/routers/resources.ts
+++ src/infra/routers/resources.ts
@@ -4,6 +4,7 @@ import {
   createOfferController,
   deleteOfferController,
   getOffersFromStoreController,
+  getResourceCategoriesController,
   getResourceOffersController,
   getSingleOfferController,
   getStoreDataController,
@@ -44,31 +45,7 @@ resourcesRouter.put('/:resourcesId/offer/:offerId/update/category', connectCateg
 
 resourcesRouter.get('/:resourcesId/offers', getResourceOffersController.handle);
 
-resourcesRouter.get('/:resourceId/categories', async (req: VerifiedTokenRequest, res: Response) => {
-  const { resourceId } = req.params as { resourceId: string };
-  try {
-    const categories = await prisma.category.findMany({
-      where: {
-        resources_id: resourceId,
-      },
-      include: {
-        sub_categories: true
-      }
-    })
-
-    return res.status(200).json({
-      status: 'success',
-      message: 'Categorias encontradas',
-      categories
-    });
-  } catch (error: any) {
-    return res.status(400).json({
-      status: 'error',
-      error: error.message,
-      message: 'Falha ao tentar criar uma nova categoria.'
-    });
-  }
-});
+resourcesRouter.get('/:resourceId/categories', getResourceCategoriesController.handle);
 
 resourcesRouter.post('/category/:categoryId/subcategory/create', async (req: VerifiedTokenRequest, res: Response) => {
   const { categoryId } = req.params as { categoryId: string };
