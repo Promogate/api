e8b764e54d4d7c95c7012d09141d66f2df6c0f71
Augusto Dias <gutodiasux@gmail.com>
2023-05-26 11:55:18 -0300

    hotfix: equal or more than 50 offer

--- src/application/utils/verify-offer-limit.ts
+++ src/application/utils/verify-offer-limit.ts
@@ -7,7 +7,7 @@ type VerifyOfferLimitParams = {
 }
 
 export const verifyOfferLimit = (input: VerifyOfferLimitParams) => {
-  if (input.is_on_showcase === true && input.role === 'FREE' && input.offerNumber === 50) {
+  if (input.is_on_showcase === true && input.role === 'FREE' && input.offerNumber >= 50) {
     throw new ErrorHandler({
       statusCode: HttpStatusCode.BAD_REQUEST,
       name: 'OfferLimitReached',
