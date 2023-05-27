d376d41a2c512d9051f74fd0a7ef455db4ad1bd2
Augusto Dias <gutodiasux@gmail.com>
2023-05-26 22:48:10 -0300

    refact: add status as a optional param in ErrorHandler class

--- src/application/utils/error-handler.ts
+++ src/application/utils/error-handler.ts
@@ -8,16 +8,19 @@ export enum HttpStatusCode {
 
 export type ErrorHandlerParams = {
   statusCode: HttpStatusCode;
+  status?: string
   message: string;
   name: string;
 }
 
 export class ErrorHandler extends Error {
   public statusCode: HttpStatusCode
+  public status: string
   public name: string
 
-  constructor ({ statusCode, message, name }: ErrorHandlerParams) {
+  constructor ({ statusCode, message, name, status }: ErrorHandlerParams) {
     super()
+    this.status = status || 'error'
     this.statusCode = statusCode
     this.name = name
     this.message = message
