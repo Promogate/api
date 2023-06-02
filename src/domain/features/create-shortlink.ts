a36f21fb603b80828368b2fc695fd7222310d0be
Augusto Dias <gutodiasux@gmail.com>
2023-06-01 21:47:37 -0300

    feat: add CreateShortlink interface

---
+++ src/domain/features/create-shortlink.ts
@@ -0,0 +1,23 @@
+export interface CreateShortlink {
+  execute(input: CreateShortlink.Input): Promise<CreateShortlink.Output>
+}
+
+export namespace CreateShortlink {
+  export type Input = {
+    fullLink: string;
+    offerId: string;
+    storeName: string;
+    resourceId: string;
+  }
+
+  export type Output = {
+    id: string;
+    code: string;
+    resourceId: string;
+    offerId: string;
+    storeName: string;
+    fullLink: string;
+    shortLink: string;
+    createdAt: string;
+  }
+}
