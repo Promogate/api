ae5987153abac2d2fecbcddd25d67b15253fa1f7
Augusto Dias <gutodiasux@gmail.com>
2023-06-04 10:50:05 -0300

    feat: add get stores route

--- src/modules/social-soul/infra/router/index.ts
+++ src/modules/social-soul/infra/router/index.ts
@@ -1,5 +1,5 @@
 import { verifyToken } from '@/application/middlewares';
-import { getCouponsController } from '@/modules/social-soul/controllers';
+import { getCouponsController, getStoresController } from '@/modules/social-soul/controllers';
 import { Router } from 'express';
 
 const socialsoulRouter = Router();
@@ -7,5 +7,7 @@ const socialsoulRouter = Router();
 socialsoulRouter.use(verifyToken);
 
 socialsoulRouter.get('/coupons', getCouponsController.handle);
+
+socialsoulRouter.get('/stores', getStoresController.handle);
 
 export { socialsoulRouter };
