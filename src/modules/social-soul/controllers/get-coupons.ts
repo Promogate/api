45cb86912bde4e28c1b7c839e788942a98ff0271
Augusto Dias <gutodiasux@gmail.com>
2023-06-04 00:18:41 -0300

    feat: add GetCouponsController

---
+++ src/modules/social-soul/controllers/get-coupons.ts
@@ -0,0 +1,26 @@
+import { ErrorHandler, HttpStatusCode } from '@/application/utils';
+import { VerifiedTokenRequest } from '@/domain/models';
+import { ConnectSocialsoulService } from '@/modules/social-soul/services';
+import { Response } from 'express';
+
+class GetCouponsController {
+  async handle(req: VerifiedTokenRequest, res: Response): Promise<Response> {
+    const { 'x-source-id': sourceId } = req.headers as { 'x-source-id': string };
+
+    const getCouponsService = new ConnectSocialsoulService({ sourceId });
+
+    try {
+      const result = await getCouponsService.getCoupons();
+
+      return res.status(200).json(result);
+    } catch (error: any) {
+      throw new ErrorHandler({
+        statusCode: HttpStatusCode.BAD_REQUEST,
+        name: error.name,
+        message: error.message
+      })
+    }
+  }
+}
+
+export const getCouponsController = new GetCouponsController();
