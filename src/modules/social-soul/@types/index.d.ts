456ffe3845a4c6b268e773bfa2a09e307f4d347d
Augusto Dias <gutodiasux@gmail.com>
2023-06-04 10:48:34 -0300

    feat: add Store types

--- src/modules/social-soul/@types/index.d.ts
+++ src/modules/social-soul/@types/index.d.ts
@@ -30,3 +30,34 @@ export type Coupon = {
   vigency: string;
   link: string;
 }
+
+export type StoresResponse = {
+  requestInfo: {
+    status: string;
+    message: string;
+  },
+  pagination: {
+    page: number;
+    size: number;
+    totalSize: number;
+    totalPage: number;
+  },
+  coupons: Store[]
+}
+
+export type Store = {
+  id: number;
+  name: string;
+  thumbnail: string;
+  link: string;
+  hasOffer: number,
+  maxCommission: number;
+  events: StoreEvent[]
+}
+
+export type StoreEvent = {
+  event: string;
+  eventType: string;
+  fixedCommission: boolean;
+  commission: number;
+}
