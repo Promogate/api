1e3d9ea4bad84c14be2a240ed3af44176304fe4b
Augusto Dias <gutodiasux@gmail.com>
2023-06-20 14:48:35 -0300

    refact: rebuild types

--- src/modules/social-soul/controllers/getStores.ts
+++ src/modules/social-soul/controllers/getStores.ts
@@ -5,7 +5,6 @@ import { Response } from 'express';
 class GetStoresController { 
   async handle(req: VerifiedTokenRequest, res: Response): Promise<Response> {
     const { 'x-source-id': sourceId } = req.headers as { 'x-source-id': string };
-
     const getStoresService = new ConnectSocialsoulService({ sourceId });
     const result = await getStoresService.getStores();
     return res.status(200).json(result);
