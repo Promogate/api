ee98125295fc1c49133b4d89bf2edceb79950e10
Augusto Dias <gutodiasux@gmail.com>
2023-06-27 16:59:34 -0300

    refact: remove redis from production

--- src/application/controllers/get-offers-from-store.ts
+++ src/application/controllers/get-offers-from-store.ts
@@ -1,5 +1,4 @@
 import { GetOffersFromStoreService } from '@/application/services';
-import { redis } from '@/infra/lib';
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
 
@@ -9,20 +8,7 @@ class GetOffersFromStoreController {
     const { store } = req.params as { store: string };
     const service = container.resolve(GetOffersFromStoreService)
     try {
-      console.time(`Execution time - ${store}`);
-      const cacheKey = store+':all';
-      const cachedOffers = await redis.get(cacheKey);
-      if (cachedOffers) {
-        console.timeEnd(`Execution time - ${store}`);
-        return res.status(200).json({
-          status: 'success',
-          message: 'Ofertas encontradas',
-          data: JSON.parse(cachedOffers)
-        })
-      }
       const result = await service.execute({ storeName: store });
-      console.timeEnd(`Execution time - ${store}`);
-      await redis.set(cacheKey, JSON.stringify(result), 'EX', 2678400)
       return res.status(200).json({
         status: 'success',
         message: 'Ofertas encontradas',
