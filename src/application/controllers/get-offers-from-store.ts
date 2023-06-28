5e1cbae839762a6c44e2832e0ac171f2062b29d1
Augusto Dias <gutodiasux@gmail.com>
2023-06-28 08:59:48 -0300

    feat: add CacheService and implement in get offers from store controller

--- src/application/controllers/get-offers-from-store.ts
+++ src/application/controllers/get-offers-from-store.ts
@@ -1,26 +1,38 @@
 import { GetOffersFromStoreService } from '@/application/services';
+import { CacheService } from '@/application/services/Cache';
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
 
 /*eslint-disable @typescript-eslint/no-explicit-any*/
 class GetOffersFromStoreController {
+
   async handle(req: Request, res: Response): Promise<Response> {
     const { store } = req.params as { store: string };
     const service = container.resolve(GetOffersFromStoreService)
-    try {
-      const result = await service.execute({ storeName: store });
-      return res.status(200).json({
-        status: 'success',
-        message: 'Ofertas encontradas',
-        data: result
-      })
-    } catch (error: any) {
-      return res.status(400).json({
-        status: 'error',
-        error: error.message,
-        message: 'Algo deu erro ao tentar buscar as ofertas'
-      })
+    const cacheService = container.resolve(CacheService)
+    const cached = await cacheService.get({ cacheKey: store+':all:offers'})
+    if (!cached) {
+      try {
+        const result = await service.execute({ storeName: store });
+        await cacheService.set({ cacheKey: store+':all:offers', content: result })
+        return res.status(200).json({
+          status: 'success',
+          message: 'Ofertas encontradas',
+          data: result
+        })
+      } catch (error: any) {
+        return res.status(400).json({
+          status: 'error',
+          error: error.message,
+          message: 'Algo deu erro ao tentar buscar as ofertas'
+        })
+      }
     }
+    return res.status(200).json({
+      status: 'success',
+      message: 'Ofertas encontradas',
+      data: cached
+    })
   }
 }
 
