f1567f700504f32b82d0516168856b18368b1a25
Augusto Dias <gutodiasux@gmail.com>
2023-07-02 20:48:57 -0300

    refact(v2): change test strategy to use use jest functions

--- tests/domain/models/User.spec.ts
+++ tests/domain/models/User.spec.ts
@@ -1,24 +1,6 @@
 import { SaveUserService } from '@/v2/application/services';
-import { SaveUserRepository } from '@/v2/data/contracts/entities';
 import { SaveUserError } from '@/v2/domain/errors';
 import { User } from '@/v2/domain/models/User';
-
-class SaveUserRepositorySpy implements SaveUserRepository {
-  user?: User
-  result: User = {
-    name: 'any_name',
-    email: 'any_email',
-    password: 'any_password',
-    agreeWithPolicies: true
-  }
-  callsCount = 0
-
-  async saveUser(input: SaveUserRepository.input): Promise<SaveUserRepository.output> {
-    this.user = input;
-    this.callsCount++
-    return this.result
-  }
-}
 
 describe('SaveUserService', function () {
   test('it should create a user entity object', function () {
@@ -27,7 +9,9 @@ describe('SaveUserService', function () {
   });
 
   test('it should call SaveUserRepository with correct params', async function () {
-    const userRepository = new SaveUserRepositorySpy();
+    const userRepository = {
+      saveUser: jest.fn()
+    };
     const sut = new SaveUserService(userRepository);
     await sut.execute({
       name: 'any_name',
@@ -35,23 +19,25 @@ describe('SaveUserService', function () {
       password: 'any_password',
       agreeWithPolicies: true
     });
-    expect(userRepository.user).toStrictEqual({
+    expect(userRepository.saveUser).toHaveBeenCalledWith({
       name: 'any_name',
       email: 'any_email',
       password: 'any_password',
       agreeWithPolicies: true
     });
-    expect(userRepository.callsCount).toBe(1);
+    expect(userRepository.saveUser).toBeCalledTimes(1);
   });
 
   test('it should return SaveUserError when user email is already registered', async function () {
-    const userRepository = new SaveUserRepositorySpy();
-    userRepository.result = {
+    const userRepository = {
+      saveUser: jest.fn()
+    };
+    userRepository.saveUser.mockResolvedValueOnce({
       name: 'any_name',
       email: 'any_email',
       password: 'any_password',
       agreeWithPolicies: true
-    }
+    })
     const sut = new SaveUserService(userRepository);
     const user = await sut.execute({
       name: 'any_name',
