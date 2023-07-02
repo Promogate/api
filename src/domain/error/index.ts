d71a63eb1764a065e85d38ffd21d8f18b3abb8ea
Augusto Dias <gutodiasux@gmail.com>
2023-07-02 19:40:05 -0300

    refact: move types to its own files

--- src/domain/error/index.ts
+++ src/domain/error/index.ts
@@ -2,7 +2,6 @@ export * from './authentication-failed';
 export * from './create-user-failed';
 export * from './refresh-token-not-found';
 export * from './save-refresh-token';
-export * from './SaveUser';
 export * from './store-name-already-exists';
 export * from './user-already-exists';
 export * from './user-not-found';
