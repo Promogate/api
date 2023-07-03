d82531afe8c1919caeefcc3a2ba8ace4883996e1
Augusto Dias <gutodiasux@gmail.com>
2023-07-02 23:42:31 -0300

    refact(v2): add FindUserByEmailRepository in SaveUserService

--- src/v2/application/services/SaveUser.ts
+++ src/v2/application/services/SaveUser.ts
@@ -1,11 +1,16 @@
-import { SaveUserRepository } from '@/v2/data/contracts/entities';
-import { SaveUserError } from '@/v2/domain/errors';
+import {
+  FindUserByEmailRepository,
+  SaveUserRepository
+} from '@/v2/data/contracts/entities';
+import { SaveUserError, UserAlreadyRegisteredError } from '@/v2/domain/errors';
 import { SaveUser } from '@/v2/domain/features';
 
 export class SaveUserService implements SaveUser {
-  constructor(private readonly repository: SaveUserRepository) { }
+  constructor(private readonly repository: SaveUserRepository & FindUserByEmailRepository) { }
 
   async execute(input: SaveUser.input): Promise<SaveUserError> {
+    const foundUser = await this.repository.findByEmail({ email: input.email });
+    if (foundUser) throw new UserAlreadyRegisteredError();
     await this.repository.saveUser(input);
     return new SaveUserError();
   }
