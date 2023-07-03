356485f40453a25996917c2dd8c87430365b84cd
Augusto Dias <gutodiasux@gmail.com>
2023-07-02 21:30:55 -0300

    refact: change sut creation strategy to use beforeEach

--- tests/domain/models/User.spec.ts
+++ tests/domain/models/User.spec.ts
@@ -1,39 +1,32 @@
 import { SaveUserService } from '@/v2/application/services';
 import { SaveUserRepository } from '@/v2/data/contracts/entities';
 import { SaveUserError } from '@/v2/domain/errors';
-import { SaveUser } from '@/v2/domain/features';
 import { User } from '@/v2/domain/models/User';
 
 import { mock, MockProxy } from 'jest-mock-extended';
 
-type SutTypes = {
-  sut: SaveUser;
-  userRepository: MockProxy<SaveUserRepository>
-}
-
-const makeSut = (): SutTypes => {
-  const userRepository = mock<SaveUserRepository>()
-  const sut = new SaveUserService(userRepository);
-  return {
-    sut,
-    userRepository
-  }
-}
-
 describe('SaveUserService', function () {
+  let userRepository: MockProxy<SaveUserRepository>;
+  let sut: SaveUserService;
+  const user= {
+    name: 'any_name',
+    email: 'any_email',
+    password: 'any_password',
+    agreeWithPolicies: true
+  }
+
+  beforeEach(function () {
+    userRepository = mock();
+    sut = new SaveUserService(userRepository);
+  })
+
   test('it should create a user entity object', function () {
     const sut = new User('any_name', 'any_email', 'any_password', true);
     expect(sut).toEqual({ name: 'any_name', email: 'any_email', password: 'any_password', agreeWithPolicies: true })
   });
 
   test('it should call SaveUserRepository with correct params', async function () {
-    const { sut, userRepository } = makeSut();
-    await sut.execute({
-      name: 'any_name',
-      email: 'any_email',
-      password: 'any_password',
-      agreeWithPolicies: true
-    });
+    await sut.execute(user);
     expect(userRepository.saveUser).toHaveBeenCalledWith({
       name: 'any_name',
       email: 'any_email',
@@ -44,19 +37,13 @@ describe('SaveUserService', function () {
   });
 
   test('it should return SaveUserError when user email is already registered', async function () {
-    const { userRepository, sut } = makeSut();
     userRepository.saveUser.mockResolvedValueOnce({
       name: 'any_name',
       email: 'any_email',
       password: 'any_password',
       agreeWithPolicies: true
     })
-    const user = await sut.execute({
-      name: 'any_name',
-      email: 'any_email',
-      password: 'any_password',
-      agreeWithPolicies: true
-    });
-    expect(user).toEqual(new SaveUserError());
+    const result = await sut.execute(user);
+    expect(result).toEqual(new SaveUserError());
   });
 })
