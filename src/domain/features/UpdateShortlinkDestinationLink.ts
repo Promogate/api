c27f94b5dc39ea4ebca5f80eb8a09f1220b019ba
Augusto Dias <gutodiasux@gmail.com>
2023-07-06 11:21:41 -0300

    feat: add UpdateShortlinkDestinationLink interface

---
+++ src/domain/features/UpdateShortlinkDestinationLink.ts
@@ -0,0 +1,19 @@
+export interface UpdateShortlinkDestinationLink {
+  execute(input: UpdateShortlinkDestinationLink.Input): Promise<UpdateShortlinkDestinationLink.Ouput>
+}
+
+export namespace UpdateShortlinkDestinationLink {
+  export type Input = {
+    destinationLink: string;
+  }
+  export type Ouput = {
+    id: string;
+    code: string;
+    short_link: string;
+    destination_link: string;
+    full_link: string;
+    resource_id: string;
+    offer_id: string;
+    store_name: string;
+  }
+}
