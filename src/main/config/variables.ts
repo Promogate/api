17b2deb1129523d0ee26daf24329a27219b2ec20
Augusto Dias <gutodiasux@gmail.com>
2023-07-17 12:11:33 -0300

    config(variables): add stripe variables do export

--- src/main/config/variables.ts
+++ src/main/config/variables.ts
@@ -3,3 +3,5 @@ export const TS_NODE_ENV = process.env.TS_NODE_ENV as string;
 export const SOCIALSOUL_APP_ID = process.env.SOCIALSOUL_APP_ID as string;
 export const SOCIALSOUL_API_URL = process.env.SOCIALSOUL_API_URL as string;
 export const UPSTASH_REDIS_URL = process.env.UPSTASH_REDIS_URL as string;
+export const STRIPE_DEV_API_KEY = process.env.STRIPE_DEV_API_KEY as string;
+export const STRIPE_LIVE_API_KEY = process.env.STRIPE_LIVE_API_KEY as string;
