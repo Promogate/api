74af8188e0cad02580b3e08817ea3515204a1ea8
Augusto Dias <gutodiasux@gmail.com>
2023-07-17 14:30:30 -0300

    test(User): ensure user is created

--- tests/domain/unit/models/User.spec.ts
+++ tests/domain/unit/models/User.spec.ts
@@ -1,8 +1,38 @@
+class User {
+  name: string
+  email: string
+  password: string
+  agreeWithPolicies: boolean
+
+  constructor(input: Input) {
+    this.name = input.name
+    this.email = input.email
+    this.password = input.password
+    this.agreeWithPolicies = input.agreeWithPolicies
+  }
+}
+
+type Input = {
+  name: string,
+  email: string,
+  password: string,
+  agreeWithPolicies: boolean
+}
+
 describe('User', () => {
   test('should create a new User', () => {
-    const sut = 'something'
-    expect(1).toBe(1);
+    const sut = new User({
+      name: 'any_name',
+      email: 'any_email',
+      password: 'any_password',
+      agreeWithPolicies: true
+    });
+    expect(sut.name).toBe('any_name');
+    expect(sut.email).toBe('any_email');
+    expect(sut.password).toBe('any_password');
+    expect(sut.agreeWithPolicies).toBe(true);
   })
+
 })
 
-export { };
+export { }
