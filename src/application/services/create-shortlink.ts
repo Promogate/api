772a41bee8c33477af7dcaf62a038a0641e7a86c
Augusto Dias <gutodiasux@gmail.com>
2023-07-24 13:34:31 -0300

    refact: remove injectable decorator from CreateShortlinkService

--- src/application/services/create-shortlink.ts
+++ src/application/services/create-shortlink.ts
@@ -1,8 +1,6 @@
 import { CreateShortlink } from '@/domain/features';
 import { pgateClient } from '@/main/config';
-import { injectable } from 'tsyringe';
 
-@injectable()
 export class CreateShortlinkService implements CreateShortlink {
   async execute(input: CreateShortlink.Input): Promise<CreateShortlink.Output> {
     const { data } = await pgateClient.post('/create', input);
