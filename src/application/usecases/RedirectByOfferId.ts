c7a5e6975ef2280751740f675295d6f63ab952d7
gutodiasux@gmail.com <gutodiasux@gmail.com>
2023-07-25 19:12:41 -0300

    feat: add RedirectByOfferIdUseCase

---
+++ src/application/usecases/RedirectByOfferId.ts
@@ -0,0 +1,19 @@
+import { FindOfferByIdRepository } from "@/data/contracts"
+import { RedirectByOfferId } from "@/domain/features"
+import { ErrorHandler, HttpStatusCode } from "@/application/utils"
+
+export class RedirectByOfferIdUseCase implements RedirectByOfferId {
+    constructor(private readonly offerRepository: FindOfferByIdRepository) {}
+
+    async execute(input: RedirectByOfferId.Input): Promise<RedirectByOfferId.Output> {
+        const result = await this.offerRepository.findOfferById({ id: input.id })
+        if (!result) throw new ErrorHandler({
+            statusCode: HttpStatusCode.NOT_FOUND,
+            name: 'OfferNotFound',
+            message: 'Redirecionamento falhou. Oferta não encontrada.'
+        })
+        return {
+            destinationLink: result.destination_link
+        }
+    }
+}
--- src/application/usecases/index.ts
+++ src/application/usecases/index.ts
@@ -1,2 +1,3 @@
 export * from './CreateOffer';
 export * from './CreateUser';
+export * from './RedirectByOfferId'
