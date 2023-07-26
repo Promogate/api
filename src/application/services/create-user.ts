53ba84908a4105bdd478f72d223f695e8a6e69b5
gutodiasux <gutodiasux@gmail.com>
2023-07-26 09:27:22 -0300

    refact: remove tsyringe decorators from CreateUserService

--- src/application/services/create-user.ts
+++ src/application/services/create-user.ts
@@ -3,11 +3,9 @@ import { CreateUser } from '@/domain/features';
 import { TOKEN_SECRET } from '@/main/config';
 import { hash } from 'bcrypt';
 import { sign } from 'jsonwebtoken';
-import { inject, injectable } from 'tsyringe';
-@injectable()
+
 export class CreateUserService implements CreateUser {
   constructor(
-    @inject('AuthenticationRepository')
     private readonly authenticationRepository: ISignUpRepo
   ) { }
 
