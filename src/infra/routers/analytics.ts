1fc23c0165ac4133c3b950be5769286f10b46c21
gutodiasux <gutodiasux@gmail.com>
2023-07-26 09:31:31 -0300

    refact: move controller logic to its own file

--- src/infra/routers/analytics.ts
+++ src/infra/routers/analytics.ts
@@ -1,59 +1,11 @@
-import { getProfileController } from '@/application/controllers';
+import { getProfileController, redirectController } from '@/application/controllers';
 import { verifyToken } from '@/application/middlewares';
 import { prisma } from '@/main/config';
 import { Request, Response, Router } from 'express';
 
 const analyticsRouter = Router();
 
-analyticsRouter.get('/redirect/offer/:id', async (req: Request, res: Response) => {
-  const { id } = req.params as { id: string };
-
-  try {
-    const offer = await prisma.offer.findUnique({
-      where: {
-        id
-      },
-      include: {
-        resources: {
-          include: {
-            analytics: {
-              select: {
-                id: true
-              }
-            }
-          }
-        }
-      }
-    })
-
-    if (!offer) {
-      return res.status(404).json({
-        status: 'error',
-        message: 'Oferta não encontrada'
-      })
-    }
-
-    await prisma.destinationClicks.create({
-      data: {
-        destination_link: offer.destination_link,
-        offer_id: id,
-        analytics_id: offer.resources.analytics?.id as string
-      }
-    })
-
-    return res.status(200).json({
-      status: 'success',
-      message: 'Oferta',
-      offer
-    })
-  } catch (error: any) {
-    return res.status(400).json({
-      status: 'error',
-      error: error.message,
-      message: 'Algo deu erro ao tentar encontrar a oferta'
-    })
-  }
-});
+analyticsRouter.get('/redirect/offer/:id', async (req, res) => await redirectController.handle(req, res));
 
 analyticsRouter.use(verifyToken);
 analyticsRouter.get('/profile/:id', getProfileController.handle);
