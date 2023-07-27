b4f87bcf04d3d5ad94ef7945d9692793e18a0911
gutodiasux <gutodiasux@gmail.com>
2023-07-27 11:41:10 -0300

    refact: change name to usecase and move to its own file

---
+++ src/application/usecases/CreateProfile.ts
@@ -0,0 +1,42 @@
+
+import {
+  ErrorHandler,
+  HttpStatusCode,
+  makeUniqueStoreName
+} from '@/application/utils';
+import { CreateProfileRepository, FindProfileByNameRepository } from '@/data/contracts';
+import { CreateProfile } from '@/domain/features';
+
+export class CreateProfileUseCase implements CreateProfile {
+  constructor(private readonly profileRepository: FindProfileByNameRepository & CreateProfileRepository) {}
+
+  async execute(input: CreateProfile.Input): Promise<CreateProfile.Output> {
+    const uniqueStoreName = makeUniqueStoreName(input.storeName);
+    const profileAlreadyExists = await this.profileRepository.checkProfile({ storeName: input.storeName })
+    if (profileAlreadyExists) {
+      throw new ErrorHandler({
+        statusCode: HttpStatusCode.BAD_REQUEST,
+        name: 'UserProfileAlreadyExists',
+        message: `Perfil já existe. (${input.storeNameDisplay} / ${uniqueStoreName})`
+      })
+    }
+    try {
+      const profile = await this.profileRepository.createProfile({ 
+        storeImage: input.storeImage,
+        storeName: uniqueStoreName,
+        storeNameDisplay: input.storeNameDisplay,
+        userId: input.userId
+      })
+
+      return {
+        profileId: profile.profileId
+      }
+    } catch (error: any) {
+      throw new ErrorHandler({
+        statusCode: error.statusCode,
+        name: error.name,
+        message: error.message
+      });
+    }
+  }
+}
--- src/application/usecases/index.ts
+++ src/application/usecases/index.ts
@@ -1,3 +1,4 @@
 export * from './CreateOffer';
+export * from './CreateProfile';
 export * from './CreateUser';
-export * from './RedirectByOfferId'
+export * from './RedirectByOfferId';
