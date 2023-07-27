1f62ee52ec44ce06f778a250d384c783a6ee1357
gutodiasux <gutodiasux@gmail.com>
2023-07-27 11:23:31 -0300

    refact: change filename to CreateProfile at controller folder

--- src/application/controllers/CreateProfile.ts
+++ src/application/controllers/CreateProfile.ts
@@ -1,4 +1,5 @@
 import { CreateProfileService } from '@/application/services';
+import { ErrorHandler, HttpStatusCode } from '@/application/utils';
 import { UserRepository } from '@/data/repositories';
 import { CreateProfile } from '@/domain/features';
 import { VerifiedTokenRequest } from '@/domain/models';
@@ -9,18 +10,25 @@ class CreateProfileController {
 
   async handle(req: VerifiedTokenRequest, res: Response): Promise<Response> {
     const body = req.body as Input;
-    const result = await this.createProfileUseCase.execute({ 
-      storeName: body.storeName,
-      storeNameDisplay: body.storeNameDisplay,
-      storeImage: body.storeImage,
-      userId: req.user as string
-     })
-
-     return res.status(200).json({
-      status: 'succcess',
-      message: 'Perfil criado com sucesso!',
-      data: result
-     })
+    try {
+      const result = await this.createProfileUseCase.execute({ 
+        storeName: body.storeName,
+        storeNameDisplay: body.storeNameDisplay,
+        storeImage: body.storeImage,
+        userId: req.user as string
+       })
+       return res.status(200).json({
+        status: 'succcess',
+        message: 'Perfil criado com sucesso!',
+        data: result
+       })
+    } catch (error: any) {
+      throw new ErrorHandler({
+        statusCode: HttpStatusCode.INTERNAL_SERVER,
+        name: 'CreateProfileControllerFailed',
+        message: error.message
+      })
+    }
   }
 }
 
