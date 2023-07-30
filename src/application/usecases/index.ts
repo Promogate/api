acd6134e1a24953f2b3cd75b07a7a62d8bd9b090
gutodiasux <gutodiasux@gmail.com>
2023-07-29 22:15:44 -0300

    refact: move SignInUseCase to its own file

--- src/application/usecases/index.ts
+++ src/application/usecases/index.ts
@@ -3,4 +3,5 @@ export * from './CreateProfile';
 export * from './CreateUser';
 export * from './GetUserInfo';
 export * from './RedirectByOfferId';
+export * from './SignIn';
 export * from './UpdateProfile';
--- tests/application/usecases/SignIn.spec.ts
+++ tests/application/usecases/SignIn.spec.ts
@@ -1,29 +1,8 @@
+import { SignInUseCase } from "@/application/usecases"
 import { FindUserByEmailIncludingPasswordRepository } from "@/data/contracts"
 import { SignInError, UserNotFoundError } from "@/domain/error"
-import { SignIn, VerifyPassword } from "@/domain/features"
-import { TOKEN_SECRET } from "@/main/config"
+import { VerifyPassword } from "@/domain/features"
 import { MockProxy, mock } from "jest-mock-extended"
-import { sign } from "jsonwebtoken"
-
-export class SignInUseCase implements SignIn {
-    constructor(
-        private readonly userRepository: FindUserByEmailIncludingPasswordRepository,
-        private readonly verifyPasswordUseCase: VerifyPassword
-      ) { }
-
-    async execute(input: SignIn.Input): Promise<SignIn.Output> {
-        const user = await this.userRepository.findByEmailIncludingPassword({ email: input.email })
-        if(!user) throw new UserNotFoundError()
-        if (!(await this.verifyPasswordUseCase.execute({ password: input.password, encryptedPassword: user.password }))) {
-            throw new SignInError()
-        }
-        const token = sign({ id: user.id, role: user.role }, TOKEN_SECRET, { expiresIn: '1d' });
-        return {
-            token
-        }
-    }
-}
-
 
 describe('SignInUseCase', () => {
     let userRepository: MockProxy<FindUserByEmailIncludingPasswordRepository>
