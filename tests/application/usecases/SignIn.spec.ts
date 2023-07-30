1864f35ccd30d8653d2defdc57da85111f152a8d
gutodiasux <gutodiasux@gmail.com>
2023-07-29 22:29:04 -0300

    test: ensure SignInUseCase return a valid token when everything is right

--- tests/application/usecases/SignIn.spec.ts
+++ tests/application/usecases/SignIn.spec.ts
@@ -1,12 +1,13 @@
 import { SignInUseCase } from "@/application/usecases"
 import { FindUserByEmailIncludingPasswordRepository } from "@/data/contracts"
 import { SignInError, UserNotFoundError } from "@/domain/error"
-import { VerifyPassword } from "@/domain/features"
+import { TokenIssuer, VerifyPassword } from "@/domain/features"
 import { MockProxy, mock } from "jest-mock-extended"
 
 describe('SignInUseCase', () => {
     let userRepository: MockProxy<FindUserByEmailIncludingPasswordRepository>
     let verifyPasswordUseCase: MockProxy<VerifyPassword>
+    let tokenIssuerUseCase: MockProxy<TokenIssuer>
     let sut: SignInUseCase
     const input = { email: 'any_email', password: 'any_password' }
     const userOutput = { id: '', password: '', email: '', role: '' }
@@ -14,7 +15,8 @@ describe('SignInUseCase', () => {
     beforeEach(() => {
         userRepository = mock()
         verifyPasswordUseCase = mock()
-        sut = new SignInUseCase(userRepository, verifyPasswordUseCase)
+        tokenIssuerUseCase = mock()
+        sut = new SignInUseCase(userRepository, verifyPasswordUseCase, tokenIssuerUseCase)
     })
 
     test('it should throw UserNotFoundError when findByEmailIncludingPassword returns undefined', async () => {
@@ -31,5 +33,13 @@ describe('SignInUseCase', () => {
         userRepository.findByEmailIncludingPassword.mockResolvedValueOnce(userOutput)
         verifyPasswordUseCase.execute.mockResolvedValueOnce(false)
         await expect(() => sut.execute(input)).rejects.toThrow(SignInError)
+    })
+
+    test('it should SignInUseCase return a valid token when everything is right', async () => {
+        userRepository.findByEmailIncludingPassword.mockResolvedValueOnce(userOutput)
+        verifyPasswordUseCase.execute.mockResolvedValueOnce(true)
+        tokenIssuerUseCase.execute.mockReturnValue({ token: 'any_token' })
+        const result = await sut.execute(input)
+        expect(result).toEqual({ token: 'any_token' })
     })
 })
